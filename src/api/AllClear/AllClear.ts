d789e4762f9a87dfa866e1024c6da95cd3f33e16
Daniel Mulroy <dan@danmulroy.com>
2023-07-30 15:10:25 +0200

    fix(allClear): Improve SMS to say everything is okay.

--- src/api/AllClear/AllClear.ts
+++ src/api/AllClear/AllClear.ts
@@ -1,59 +1,71 @@
 // Docs on event and context https://docs.netlify.com/functions/build/#code-your-function-2
 import { HandlerEvent } from "@netlify/functions";
-import { MEMBERS, EXTRACT_GET_AND_POST_PARAMS_FROM_EVENT, joiner } from "../lib";
+import {
+	MEMBERS,
+	EXTRACT_GET_AND_POST_PARAMS_FROM_EVENT,
+	joiner,
+} from "../lib";
 import { Imember } from "../lib/member";
 import { sendSMS } from "../lib/sendSMS";
 
-const { parse } = require('querystring');
+const { parse } = require("querystring");
 
-console.log("MEMBERS",MEMBERS)
-export const MessageAllMembers = async (MEMBERS_TO_MESSAGE : Array<Imember>, BUILD_MESSAGE:Function) => {
-  try {
-    let messages = MEMBERS_TO_MESSAGE
-      .map(async member => {
-        const { phone_number } = member;        
-        let to = member.phone_number;
-        return await sendSMS(phone_number, BUILD_MESSAGE(member))
-    });
-    
-    return await Promise.all(messages)
-    .then((results) => {
-      console.log(results)
-       return {
-         statusCode: 200,
-        //  headers: { "content-type": "text/xml" },
-         body: `${results.length} Membres prévenu.`
-       };
-    })
-    .catch((e) => {
-      console.error(e)
-      return {
-        statusCode: 500,
-        //  headers: { "content-type": "text/xml" },
-         body: e.message
-    }
-   });
+console.log("MEMBERS", MEMBERS);
+export const MessageAllMembers = async (
+	MEMBERS_TO_MESSAGE: Array<Imember>,
+	BUILD_MESSAGE: Function
+) => {
+	try {
+		let messages = MEMBERS_TO_MESSAGE.map(async (member) => {
+			const { phone_number } = member;
+			let to = member.phone_number;
+			return await sendSMS(phone_number, BUILD_MESSAGE(member));
+		});
 
-  } catch (error:any) {
-    console.error(error)
-    return { statusCode: 500, body: error.toString() }
-  }
-}
+		return await Promise.all(messages)
+			.then((results) => {
+				console.log(results);
+				return {
+					statusCode: 200,
+					//  headers: { "content-type": "text/xml" },
+					body: `${results.length} Membres prévenu.`,
+				};
+			})
+			.catch((e) => {
+				console.error(e);
+				return {
+					statusCode: 500,
+					//  headers: { "content-type": "text/xml" },
+					body: e.message,
+				};
+			});
+	} catch (error: any) {
+		console.error(error);
+		return { statusCode: 500, body: error.toString() };
+	}
+};
 
-export const AllClear = async (event:HandlerEvent) => {
-  console.log("AllClear starting...")
-  const PARAMS = EXTRACT_GET_AND_POST_PARAMS_FROM_EVENT(event);
-  let RESPONSIBLE_PARTY:Imember = MEMBERS[PARAMS.mid];
-  console.log(RESPONSIBLE_PARTY, MEMBERS)
-  // trigger success condition
-  console.log(RESPONSIBLE_PARTY.fname,"has said they are handling it! Hurrah! Messaging",MEMBERS.length,"people...")
-  
-  let BUILD_MESSAGE = ({ fname = "Cher ami"}) => `${fname}, nous vous avons appellé car une alarme a été déclenché chez Marie Francoise de Pitray. Entretemps, ${RESPONSIBLE_PARTY.fname} ${RESPONSIBLE_PARTY.lname} a accepté de s'en occuper.`;
-  let MEMBERS_TO_MESSAGE = MEMBERS
-    .filter((m:Imember) => joiner(m) !== joiner(MEMBERS[PARAMS.mid]) ) // filtering on phone number didn't work if  I wanted to use my own on multiple members for testing
+export const AllClear = async (event: HandlerEvent) => {
+	console.log("AllClear starting...");
+	const PARAMS = EXTRACT_GET_AND_POST_PARAMS_FROM_EVENT(event);
+	let RESPONSIBLE_PARTY: Imember = MEMBERS[PARAMS.mid];
+	console.log(RESPONSIBLE_PARTY, MEMBERS);
+	// trigger success condition
+	console.log(
+		RESPONSIBLE_PARTY.fname,
+		"has said they are handling it! Hurrah! Messaging",
+		MEMBERS.length,
+		"people..."
+	);
 
-  console.log(BUILD_MESSAGE(RESPONSIBLE_PARTY), MEMBERS_TO_MESSAGE)
-  return await MessageAllMembers(MEMBERS_TO_MESSAGE, BUILD_MESSAGE)
-}
+	let BUILD_MESSAGE = ({ fname = "Cher ami" }) =>
+		`${fname}, une alarme a été déclenché chez Marie Francoise de Pitray, mais heureusement ${RESPONSIBLE_PARTY.fname} ${RESPONSIBLE_PARTY.lname} déclare que tout va bien.`;
+	let MEMBERS_TO_MESSAGE = MEMBERS.filter(
+		(m: Imember) => joiner(m) !== joiner(MEMBERS[PARAMS.mid])
+	); // filtering on phone number didn't work if  I wanted to use my own on multiple members for testing
 
-export const handler = module.exports.AllClear
+	console.log(BUILD_MESSAGE(RESPONSIBLE_PARTY), MEMBERS_TO_MESSAGE);
+	return await MessageAllMembers(MEMBERS_TO_MESSAGE, BUILD_MESSAGE);
+};
+
+export const handler = module.exports.AllClear;
